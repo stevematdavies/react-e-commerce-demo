639a7bbde703714585bd4b969eb8f153ab425998
stevematdavies <stevematdavies@gmail.com>
2020-09-24 15:20:25 +0300

    adding header component and basic navigation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop/shop-page.component'
 
 import './App.css';
+import Header from "./componeents/header/header.component";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
