587602802528f5e4e4f030e80fe3cd88e9f7033c
stevematdavies <stevematdavies@gmail.com>
2020-09-24 10:20:16 +0300

    first commit

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DirectoryComponent from "../componeents/directory/directory.component";
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <DirectoryComponent/>
+  </div>
+);
+
+export default HomePage;
