11aef2d297f105e6696d7404873d80bae48f17c3
stevematdavies <stevematdavies@gmail.com>
2020-09-25 17:24:10 +0300

    adding dispatch shortcut

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,10 +5,11 @@ import CustomButton from "../custom-button/custom-button";
 import {createStructuredSelector} from "reselect";
 import {selectCartItems} from '../../redux/cart/cart.selectors'
 import CartItem from "../cart-item/cart-item.component";
+import {toggleShoppingCart} from "../../redux/cart/cart.actions";
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = ({visible, cartItems, history}) => (
+const CartDropdown = ({visible, cartItems, history, dispatch}) => (
   <div className={`cart-dropdown ${visible ? '' : 'hidden'}`}>
     <div className="cart-items">
       {
@@ -17,14 +18,17 @@ const CartDropdown = ({visible, cartItems, history}) => (
           : <span className="empty-message">Your shopping cart is empty</span>
       }
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>
+    <CustomButton onClick={() => {
+      history.push('/checkout')
+      dispatch(toggleShoppingCart())
+    }}>
       GO TO CHECKOUT
     </CustomButton>
   </div>
 )
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems
+  cartItems: selectCartItems,
 })
 
 export default withRouter(connect(mapStateToProps)(CartDropdown))
