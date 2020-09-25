177838baf9e9fedf3809e85faa6ad6e7d6304837
stevematdavies <stevematdavies@gmail.com>
2020-09-25 15:40:04 +0300

    updating class name

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,7 +5,7 @@ import './cart-item.styles.scss'
 const CartItem = ({item: {imageUrl, price, name, quantity}}) => (
   <div className="cart-item">
     <img src={imageUrl} alt={name}/>
-    <div className="image-detail">
+    <div className="item-details">
       <span className="name">{name}</span>
       <span className="price">
         {quantity} x ${price}
