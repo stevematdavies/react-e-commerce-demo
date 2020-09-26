8233174b213fc64e71cee8a53aa016de7cf9d0b6
stevematdavies <stevematdavies@gmail.com>
2020-09-26 15:36:12 +0300

    adding sections to state

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -18,7 +18,7 @@ const CheckoutItem = ({item, clearItem, removeItem, addItem}) => {
         <span className="value">{quantity}</span>
       <div className="arrow" onClick={() => addItem(item)}>&#10095;</div>
     </span>
-      <span className="price">{price}</span>
+      <span className="price">${price}</span>
       <div className="remove-button" onClick={() => clearItem(item)}>&#10005;</div>
     </div>
   )
---
+++ src/redux/directory/directory.selectors.js
@@ -0,0 +1,9 @@
+import {createSelector} from "reselect";
+
+
+const selectDirectory = ({directory}) => directory
+
+export const selectDirectorySections = createSelector(
+  [selectDirectory],
+  ({sections}) => sections
+)
