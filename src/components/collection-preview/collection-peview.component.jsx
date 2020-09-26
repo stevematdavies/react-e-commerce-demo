1eb8f7cdade45278a336e017644cbfa50095ccaf
stevematdavies <stevematdavies@gmail.com>
2020-09-26 18:03:47 +0300

    asdding linking to preview

--- src/components/collection-preview/collection-peview.component.jsx
+++ src/components/collection-preview/collection-peview.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import {withRouter} from "react-router";
 import CollectionItem from "../collection-item/collection-item.component";
 
 import './collection-preview.styles.scss'
 
-const CollectionPreview = ({title, items}) => (
-  <div className="collection-preview">
+const CollectionPreview = ({title, items, history, match}) => (
+  <div
+    className="collection-preview"
+    onClick={() => history.push(`${match.url}/${title.toLowerCase()}`)}>
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {
@@ -14,6 +17,6 @@ const CollectionPreview = ({title, items}) => (
   </div>
 )
 
-export default CollectionPreview
+export default withRouter(CollectionPreview)
 
 
