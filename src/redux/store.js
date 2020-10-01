bd8fd7893c16f4dec654203a3ffa38e3c0d84b0f
stevematdavies <stevematdavies@gmail.com>
2020-10-01 11:02:23 +0300

    adding logger for prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { persistStore } from "redux-persist";
 import logger from 'redux-logger'
 import rootReducer from './root-reducer'
 
-const middlewares = [logger]
+const middlewares = process.env.NODE_ENV === 'development' ? [logger] : []
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 export const persistedStore = persistStore(store)
